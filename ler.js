e1880d13d78b8e31f4a474b39637dc76e0f6b3c2
danieldeclink <daniel@declink.com.br>
2024-08-30 14:32:14 -0300

    atualização do cdVistoria

--- ler.js
+++ ler.js
@@ -1,6 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const axios = require('axios');
 
+let id = process.argv[2]
+
 // Conectar ao banco de dados (substitua 'your_database.db' pelo nome do seu arquivo .db)
 let db = new sqlite3.Database('SJC_VISTORIA.db', (err) => {
     if (err) {
@@ -12,30 +14,31 @@ let db = new sqlite3.Database('SJC_VISTORIA.db', (err) => {
 
 // Executar uma consulta SQL (substitua 'your_table' pelo nome da sua tabela)
 db.serialize(() => {
-  db.each(`SELECT * FROM TBL_VISTORIA_DETALHES`, (err, row) => {
+    db.each(`SELECT * FROM TBL_VISTORIA_DETALHES WHERE idVistoria = ?`, [id], (err, row) =>  {
       if (err) {
           console.error(err.message);
       }
       // // Construir a URL com o ID como parâmetro
-      // const url = `http://131.255.236.50:8080/WsDecSgt/VistoriaSJC/GravaDetalhes?data=${row.id}`; // Substitua pela sua URL
+      const url = `http://131.255.236.50:8080/WsDecSgt/VistoriaSJC/GravaDetalhes?data=${montaJson(row)}`; // Substitua pela sua URL
 
-      // // // Fazer a requisição HTTP usando axios
-      // // axios.get(url)
-      // //     .then(response => {
-      // //         console.log(`Response for ID ${row.id}:`, response.data);
-      // //     })
-      // //     .catch(error => {
-      // //         console.error(`Error fetching data for ID ${row.id}:`, error.message);
-      // //     });
-      console.log(montaJson(row));
+      // // Fazer a requisição HTTP usando axios
+    //   axios.get(url)
+    //       .then(response => {
+    //           console.log(`Response for ID ${row.idVistoria}:`, response.data);
+    //       })
+    //       .catch(error => {
+    //           console.error(`Error fetching data for ID ${row.idVistoria}:`, error.message);
+    //       });
+      console.log(montaJson(row)) ;
   });
 });
 
 function montaJson(row){
     let json = "";
-    json+= {
-    "cdGrupo":2,"hrFiscalizacao":0,"obsItem":"","cdItem":1787,"idVistoria":183,"cdVistoria":19124,"flItem":1,"prazoRegularizacao":"3 dias","_id":951,"transmitido":false,"cdSubGrupo":272
-    }
+    json+= `{"cdGrupo":${row.cdGrupo},"hrFiscalizacao":${row.hrFiscalizacao},"obsItem":${row.obsItem.replace(/(\r\n|\n|\r)/gm, " ")
+    },"cdItem":${row.cdItem},"idVistoria":${row.idVistoria},"cdVistoria":19200,"flItem":${row.flItem},"prazoRegularizacao":${row.prazoRegularizacao},"_id":${row._id},"transmitido":${row.transmitido},"cdSubGrupo":${row.cdSubGrupo}}`
+    
+
 
     return json
 }
